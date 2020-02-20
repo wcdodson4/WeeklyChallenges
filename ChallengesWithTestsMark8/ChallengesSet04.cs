714a02835ed2c3a35bdffa24b01995958a55e972
William Dodson <wcdodson4@gmail.com>
2020-02-20 11:33:50 -0600

    set 4 completed

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,97 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers.Length == 0)
+                return 0;
+            var total = 0;
+            foreach(var num in numbers)
+            {
+                if (num % 2 == 0)
+                    total += num;
+                if (num % 2 != 0)
+                    total -= num;
+            }
+            return total;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            list.Add(str1.Length);
+            list.Add(str2.Length);
+            list.Add(str3.Length);
+            list.Add(str4.Length);
+            return list.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            list.Add(number1);
+            list.Add(number2);
+            list.Add(number3);
+            list.Add(number4);
+            return list.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            var sideList = new List<int>();
+            sideList.AddRange(new List<int>() {sideLength1, sideLength2, sideLength3});
+            var orderedList = sideList.OrderByDescending(n => n).ToArray();
+            if (orderedList[2] + orderedList[1] > orderedList[0])
+                return true;
+            else
+                return false;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return Double.TryParse(input, out Double result);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var nulls = objs.Where(n => n == null);
+            if (nulls.Count() > (objs.Length / 2))
+                return true;
+            else
+                return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+                return 0;
+            var evens = new List<int>();
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                    evens.Add(num);
+            }
+            if (evens.Count == 0 || evens == null)
+                return 0;
+            return evens.Average();
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0)
+                return 1;
+            if (number < 0)
+                throw new ArgumentOutOfRangeException();
+            var fact = 1;
+            while (number != 1)
+            {
+                fact = fact * number;
+                number = number - 1;
+            }
+            return fact;
         }
     }
 }
